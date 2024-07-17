524cbecc0960d7a6fbac3bb74ef8214cebd3a3f9
artem.borodkin@gmail.com <artem.borodkin@gmail.com>
2024-07-17 15:38:38 +0300

    update

--- listener/src/server.rb
+++ listener/src/server.rb
@@ -8,7 +8,7 @@ class StackManagerApi < Grape::API
   helpers UpdateService
 
   format :json
-  content_type :json, ['application/json', 'application/vnd.docker.distribution.events.v1+json']
+  # content_type :json, 'application/json'
 
   post '/on_registry_events', &-> { on_registry_events params[:events] }
   post '/update_services', &-> { update_services params[:image], params[:digest] }
@@ -16,14 +16,7 @@ class StackManagerApi < Grape::API
   get '/healthcheck', &-> {  } # LOGGER.debug :healthcheck
 end
 
-# Monkeypatch rack-cache to allow for multiple mime types (json)
-# Example: content_type :json, ['application/json', 'application/vnd.docker.distribution.events.v1+json']
+# always return json mime type
 class Grape::Middleware::Base
-  def mime_types
-    @mime_types ||= content_types.each_pair.with_object({}) do |(k, v), types_without_params|
-      [v].flatten.each do |vv|
-        types_without_params[vv.split(';').first] = k
-      end
-    end
-  end
+  def mime_types = ->(_){ :json }
 end
