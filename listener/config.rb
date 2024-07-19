e5a0ec16003702c9d636fbc00a8e43334c181c0a
artem.borodkin@gmail.com <artem.borodkin@gmail.com>
2024-07-19 13:33:28 +0300

    update

--- listener/config.rb
+++ listener/config.rb
@@ -7,11 +7,11 @@ IMAGE_FILTER = Regexp.new(ENV['IMAGE_FILTER'] || "127\.0\.0\.1:5000") # RegExp 1
 HOSTS = ENV['DOCKER_HOSTS'].to_s.split ',' # unix:///var/run/docker.sock
 
 raise 'DOCKER_HOSTS not set' if HOSTS.empty?
-LOGGER.warn "TELEGRAM TOKEN, CHAT_ID not set" unless ENV['TELEGRAM_BOT_TOKEN'] && ENV['TELEGRAM_CHAT_ID']
+LOGGER.warn 'TELEGRAM TOKEN, CHAT_ID not set' unless ENV['TELEGRAM_BOT_TOKEN'] && ENV['TELEGRAM_CHAT_ID']
 
 HOSTS = HOSTS.map do |host|
   ctx = "ctx-#{host.gsub(%r{^ssh://|^unix://}, '').gsub(/[@:.\/]/, '-')}"
-  exec_ "docker context create #{ctx} --docker \"host=#{host}\"" \
+  exec_ %(docker context create #{ctx} --docker "host=#{host}") \
     unless exec_('docker context ls --format {{.Name}}').include? ctx
   name = exec_ %(docker --context #{ctx} info | grep Name | sed ' s/Name: //')
   [ctx, Async::Semaphore.new(1), host, name]
