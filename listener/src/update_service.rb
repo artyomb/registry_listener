663f43b7fa2d349bd28bc1b88b98a5a127bde7eb
astrot1988 <astrot1988@gmail.com>
2024-08-01 10:39:32 +0300

    wip: notify error

--- listener/src/update_service.rb
+++ listener/src/update_service.rb
@@ -2,9 +2,30 @@ require 'async'
 require 'async/semaphore'
 require 'open3'
 
+# list docker service tasks
+# docker service ps graph_node_graph_solve_api --filter="desired-state=running" --no-trunc --format="{{ .ID }}"
+
+# docker task container
+# docker inspect 1uu75an0sp169tkgezslmi9dn | jq -r .[0].Status.ContainerStatus.ContainerID
+
+# container image id
+# docker container inspect bafa6b18af7a394113e761312859e27ca27c33c8bfe3e41c9deb8f87fa4b53ca | jq -r .[0].Image
+
+# docker task image sha
+# docker container inspect $(docker inspect 1uu75an0sp169tkgezslmi9dn | jq -r .[0].Status.ContainerStatus.ContainerID) | jq -r .[0].Image
+
+
 module UpdateService
   extend self
   UPDATE = Async::Semaphore.new(1)
+
+  async otl_def def list_service_tasks(ctx, service_name)
+    exec_("docker --context #{ctx} service ps --filter=\"desired-state=running\" --no-trunc --format \"{{.ID}}\" #{service_name}").lines.map(&:strip).uniq
+  end
+
+  async otl_def def get_task_image_digest(ctx, task_name)
+    exec_("docker  --context #{ctx} container inspect $(docker inspect #{task_name} | jq -r .[0].Status.ContainerStatus.ContainerID) | jq -r .[0].Image")
+  end
 
   async otl_def def list_services(ctx)
     exec_("docker --context #{ctx} service ls --format {{.Name}}\\\|{{.Image}}").lines.map { _1.split('|').map(&:strip) }.to_h
@@ -47,6 +68,11 @@ module UpdateService
                 next "Skipping(update_image) #{c_name}: #{service_image}" unless service_image =~ /#{update_image}/
               else
                 next "Skipping: #{service_image}" unless service_image =~ IMAGE_FILTER
+              end
+              task_names = list_service_tasks(ctx, name).wait
+              task_names.each do |task_name|
+                digest = get_task_image_digest(ctx, task_name).wait
+                p "#{c_name}: #{name} #{service_image} on #{_host} digest: #{digest}"
               end
 
               latest_digest, service_digest = [image_digest(ctx, service_image), service_image_digest(ctx, name)].map(&:wait)
